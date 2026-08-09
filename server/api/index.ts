c85bef44c03e52a0ef7bb993ee665f6e1089475c
batoolarifa <batoolarifa73@gmail.com>
2026-08-09 10:32:57 +0500

    Configure backend for Vercel deployment

---
+++ server/api/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { app } from "../app";
+import connectDB from "../utils/db";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
+
+connectDB();
+
+export default app;
