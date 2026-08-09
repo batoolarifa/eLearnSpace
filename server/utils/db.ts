e3b6e9e42ef929d2be4ab37161a14619635168cb
batoolarifa <batoolarifa73@gmail.com>
2026-08-09 21:24:32 +0500

    fix: remove manual DNS server override causing MongoDB SRV lookup timeout

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,5 +1,5 @@
-const dns = require("dns"); 
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// const dns = require("dns"); 
+// dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 
 import mongoose from "mongoose";
