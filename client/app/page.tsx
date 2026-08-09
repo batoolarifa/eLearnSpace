695074e101c976fad03b1b22033f6d6cf2c8edf5
batoolarifa <batoolarifa73@gmail.com>
2026-08-09 12:35:11 +0500

    test: verify Next.js homepage deployment

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,39 +1,47 @@
 'use client';
 
-import { FC, useState } from "react";
-import Header from "./components/Header";
-import Hero from "./components/Route/Hero";
-import Courses  from "./components/Route/Courses";
-import Reviews from "./components/Route/Reviews";
-import FAQ from "./components/FAQ/FAQ";
-import Footer from "./components/Footer"
 
-const Page: FC = () => {
-  const [open, setOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState(0)
-  const [route, setRoute] = useState("Login");
-  return (
-    <div>
-    <Header
-      open={open}
-      setOpen={setOpen}
-      activeItem={activeItem}
-      setRoute={setRoute}
-      route={route}
-    />
+export default function Page() {
+  return <h1>eLearnSpace Frontend Works</h1>;
+}
 
-    <Hero />
-    <Courses />
-    <Reviews />
-    <FAQ />
-    <Footer />
+
+
+
+// import { FC, useState } from "react";
+// import Header from "./components/Header";
+// import Hero from "./components/Route/Hero";
+// import Courses  from "./components/Route/Courses";
+// import Reviews from "./components/Route/Reviews";
+// import FAQ from "./components/FAQ/FAQ";
+// import Footer from "./components/Footer"
+
+// const Page: FC = () => {
+//   const [open, setOpen] = useState(false);
+//   const [activeItem, setActiveItem] = useState(0)
+//   const [route, setRoute] = useState("Login");
+//   return (
+//     <div>
+//     <Header
+//       open={open}
+//       setOpen={setOpen}
+//       activeItem={activeItem}
+//       setRoute={setRoute}
+//       route={route}
+//     />
+
+//     <Hero />
+//     <Courses />
+//     <Reviews />
+//     <FAQ />
+//     <Footer />
       
      
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
 
 
 
