370de94e63f6d26cd4b84e0bae99b9ecad6e15c4
batoolarifa <batoolarifa73@gmail.com>
2026-08-09 17:31:34 +0500

    deploy backend from server directory

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,47 +1,41 @@
 'use client';
 
 
-export default function Page() {
-  return <h1>eLearnSpace Frontend Works</h1>;
-}
 
+import { FC, useState } from "react";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+import Courses  from "./components/Route/Courses";
+import Reviews from "./components/Route/Reviews";
+import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer"
 
+const Page: FC = () => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0)
+  const [route, setRoute] = useState("Login");
+  return (
+    <div>
+    <Header
+      open={open}
+      setOpen={setOpen}
+      activeItem={activeItem}
+      setRoute={setRoute}
+      route={route}
+    />
 
-
-// import { FC, useState } from "react";
-// import Header from "./components/Header";
-// import Hero from "./components/Route/Hero";
-// import Courses  from "./components/Route/Courses";
-// import Reviews from "./components/Route/Reviews";
-// import FAQ from "./components/FAQ/FAQ";
-// import Footer from "./components/Footer"
-
-// const Page: FC = () => {
-//   const [open, setOpen] = useState(false);
-//   const [activeItem, setActiveItem] = useState(0)
-//   const [route, setRoute] = useState("Login");
-//   return (
-//     <div>
-//     <Header
-//       open={open}
-//       setOpen={setOpen}
-//       activeItem={activeItem}
-//       setRoute={setRoute}
-//       route={route}
-//     />
-
-//     <Hero />
-//     <Courses />
-//     <Reviews />
-//     <FAQ />
-//     <Footer />
+    <Hero />
+    <Courses />
+    <Reviews />
+    <FAQ />
+    <Footer />
       
      
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
 
 
 
